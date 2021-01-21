ba3b7f1c4db4c4640c48fcf7a56416ff4e201384
salad <fmoenne-@student.le-101.fr>
2021-01-21 13:05:34 +0100

    forks is now on the stack, we're going somewhere

--- philo-one/src/philo_one.c
+++ philo-one/src/philo_one.c
@@ -12,6 +12,7 @@ ph_act(void *ptr)
 	t_philo *ph;
 
 	ph = (t_philo*)ptr;
+	printf("num is acting :[%d]\n", ph->num);
 	ph_eat(ph);
 	ph_sleep(ph);
 	return (ptr);
@@ -20,17 +21,19 @@ ph_act(void *ptr)
 void
 ph_start(t_shared *sh)
 {
+	pthread_mutex_t forks[*sh->max_ph];
 	pthread_t pt;
 	t_philo **pht;
 	int i;
 
 	i = 0;
 	pht = (t_philo **)malloc((sizeof(t_philo*) * *sh->max_ph));
+	sh->forks = (pthread_mutex_t**)&forks;
 	while (i < *sh->max_ph)
 	{
 		pht[i] = (t_philo *)malloc(sizeof(t_philo));
 		pht[i]->lfork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-		/* pht[i]->rfork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)); */
+		pht[i]->rfork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
 		pht[i]->num = i + 1;
 		pht[i]->shared = sh;
 		i++;
@@ -41,15 +44,15 @@ ph_start(t_shared *sh)
         printf("[%d]", i);
 		pthread_mutex_init(pht[i]->lfork, NULL);
 		pht[i]->lfork = pht[i]->shared->forks[i];
-		/* pthread_mutex_init(pht[i]->rfork, NULL); */
-		/* pht[i]->rfork = (i == (*sh->max_ph - 1)) ? pht[i]->shared->forks[0] : */
-        /*     pht[i]->shared->forks[i + 1]; */
+		pthread_mutex_init(pht[i]->rfork, NULL);
+		pht[i]->rfork = (i == (*sh->max_ph - 1)) ? pht[i]->shared->forks[0] :
+            pht[i]->shared->forks[i + 1];
 		pthread_create(&pt, NULL, ph_act, pht[i]);
 		i++;
 	}
 	pthread_join(pt, NULL);
-	pthread_mutex_destroy(pht[0]->lfork);
-	/* pthread_mutex_destroy(pht[i]->rfork); */
+	/* pthread_mutex_destroy(&pht[0]->lfork); */
+	/* pthread_mutex_destroy(&pht[i]->rfork); */
 }
 
 static short
@@ -65,12 +68,8 @@ ph_init(int ac, char *av[], t_shared *sh)
 		i++;
 	}
 	ph_fills(ac, av, sh);
-	if (!(sh->forks = (pthread_mutex_t **)malloc(sizeof(pthread_mutex_t*)* *sh->max_ph)))
-		return (-1);
     while (i < *sh->max_ph)
     {
-        if ((!(sh->forks[i] = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)))))
-            return (-1);
         i++;
 	}
 	return (0);
