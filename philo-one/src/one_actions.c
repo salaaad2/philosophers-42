e29fc9822f1856c026e32a91daed102f08b26230
salad <fmoenne-@student.le-101.fr>
2021-01-26 19:27:22 +0100

    seems consistent

--- philo-one/src/one_actions.c
+++ philo-one/src/one_actions.c
@@ -15,18 +15,24 @@ ph_eat(t_philo *ph)
     struct timeval ctv;
 
     gettimeofday(&ctv, NULL);
-    /* printf("\n[%ld]%d is waiting for mutex to be unlocked", ph_timest(1, (ctv.tv_sec * 1000) + */
-    /*     (ctv.tv_usec / 1000)), ph->num); */
+    if ((ph_timest(1, (ctv.tv_sec * 1000) +
+        (ctv.tv_usec / 1000)) - ph->lastate) > *ph->shared->time_to_die)
+    {
+        printf("this ones dead[%ld], [%d]\n", ph->lastate, *ph->shared->time_to_die);
+        return (-1);
+    }
     pthread_mutex_lock(ph->lfork);
     pthread_mutex_lock(ph->rfork);
     gettimeofday(&ctv, NULL);
     printf("\n[%ld]%d is eating", ph_timest(1, (ctv.tv_sec * 1000) +
         (ctv.tv_usec / 1000)), ph->num);
+    usleep(*ph->shared->time_to_eat * 1000);
+    gettimeofday(&ctv, NULL);
     ph->lastate = ph_timest(1, (ctv.tv_sec * 1000) +
         (ctv.tv_usec / 1000));
-    usleep(*ph->shared->time_to_eat * 1000);
     pthread_mutex_unlock(ph->lfork);
     pthread_mutex_unlock(ph->rfork);
+    gettimeofday(&ctv, NULL);
     printf("\n[%ld]%d stopped eating", ph_timest(1, (ctv.tv_sec * 1000) +
         (ctv.tv_usec / 1000)), ph->num);
     return (0);
